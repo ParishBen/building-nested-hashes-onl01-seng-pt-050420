cd3a93b4d6ed5ab1978f88bf068cde2a2a0fd1cf
Ben Parish <parishben13@gmail.com>
2020-05-13 02:51:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => [{name: "Benvolio",age:"17",attitude:"worried"},
-      {}]
+      {name:"Mercutio",age:"18",attitude:"hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"
